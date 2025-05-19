68d0676fdab6f390160b9c4a7cdb9dd969f8f0c3
hypernova666 <matteo.depoi@alessandrinimainardi.edu.it>
2025-05-19 13:47:02 +0200

    Add files via upload

---
+++ verifica!.py
@@ -0,0 +1,18 @@
+tupla_vendite = (
+    ("Telefono", 500, 3, "2023-10-01"),
+    ("Computer", 1200, 2, "2023-10-02"),
+    ("Monitor", 300, 5, "2023-10-03"),
+    ("Cuffie", 80, 10, "2023-10-04"),
+    ("Smartwatch", 200, 7, "2023-10-05"),
+    ("Tablet", 400, 4, "2023-10-06"),
+)
+
+def totale_incassi(tupla_vendite):
+    for i in range (len(tupla_vendite)):                                                                                                                                                                            
+        sum=(i,(1,2))                                                                                                                                                                                                   
+        print(sum)
+totale_incassi(tupla_vendite)
+#def media_prezzo(tupla_vendite,prodotto):
+#    for i in range (tupla_vendite):
+#        prodotto=tupla_vendite(i,1)
+#media_prezzo(tupla_vendite,prodotto)    
